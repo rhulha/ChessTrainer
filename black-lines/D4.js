64809885a59c7c50b6f9fa0d001d365ba002ecf1
java4life <java4life@gmail.com>
2026-03-19 20:40:41 +0100

    feat: add new chess lines and implement HTML structure for black gameplay

---
+++ black-lines/D4.js
@@ -0,0 +1,30 @@
+export var E5 = {
+ // https://www.youtube.com/watch?v=oksV18QmCwo
+ Derek_Wu_Trap_as_black: "1. d4 e5 2. dxe5 Bc5 3. Nf3 d6 4. exd6 Ne7 5. dxe7 Bxf2+ 6. Kxf2 Qxd1",
+}
+
+export var C5 = {
+ // https://youtu.be/6187BUdcK34
+ RCA: "1. d4 c5 2. dxc5 e6 3. b4 a5 4. c3 axb4 5. cxb4 Qf6 6. Nc3 Qxc3+ 7. Bd2 Qa3 8. g3 Nf6"
+}
+
+export var against_queens_gambit = {
+ // https://www.youtube.com/watch?v=6r_VKSdpRH8
+ elephant_trap: "1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. cxd5 exd5 5. Bg5 Nbd7 6. Nxd5 Nxd5 7. Bxd8 Bb4+ 8. Qd2 Bxd2+ 9. Kxd2 Kxd8 10. e4 Nb4 11. f3",
+
+// https://www.youtube.com/watch?v=6r_VKSdpRH8
+  elephant_trap_no_take: "1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bg5 Nbd7 5. e3 c6 6. Nf3 Qa5 7. Bd3 dxc4 8. Bxc4 Ne4 9. Qc2 (9. Bh4 Nxc3 10. bxc3 Qxc3+ 11. Nd2 Bb4 12. Rc1 Qxd2+ 13. Qxd2 Bxd2+ 14. Kxd2) 9... Nxg5 10. Nxg5 Qxg5",
+
+ albin_countergambit: "1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. cxd5 exd5 5. Bg5 Nbd7 6. Nxd5 Nxd5 7. Bxd8 Bb4+ 8. Qd2 Bxd2+ 9. Kxd2 Kxd8 10. e4 Nb4 11. f3",
+
+ promote_to_knight: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. e3 Bb4+ 5. Bd2 dxe3 6. Bxb4 exf2+ 7. Ke2 fxg1=N+ 8. Rxg1 Bg4+ 9. Kf2 Qxd1 10. Bc3 Nd7 11. h3 Nc5",
+
+ cambridge_springs: "",
+
+ budapest_gambit: "",
+
+  // https://www.youtube.com/watch?v=7QNPWtVKaNg
+ fajarowicz_gambit: "1. d4 Nf6 2. c4 e5 3. dxe5 Ne4 4. f3 Bb4+ 5. Bd2 Qh4+ 6. g3 Nxg3 7. Bxb4 Ne4#"
+  
+}
+
---
+++ black-lines/E4.js
@@ -0,0 +1,29 @@
+// https://lichess.org/ss9vyh4xfQfW
+
+export var against_caro_kann = {
+  heavy_line:
+    '1. e4 c6 2. d4 d5 3. e5 Bf5 4. Ne2 e6 5. Nf4 Nd7 6. g4 Bg6 7. h4 h6 8. Nxg6 fxg6 9. Bd3 Ne7 10. Nc3 Qb6 11. Ne2 O-O-O 12. c3 c5 13. Kf1 h5 14. gxh5 gxh5 15. Bg5 g6',
+};
+
+export var E5 = {
+  turkish_gambit:
+    '1. e4 e5 2. Nf3 d5 3. exd5 e4 4. Qe2 Nf6 5. d3 Qxd5 6. Nc3 Bb4 7. Bd2 Bxc3 8. Bxc3 O-O 9. Bxf6 exf3 10. Qxf3 Qxf3 11. gxf3 Re8+ 12. Kd2 gxf6',
+
+  against_italian:
+    '1. e4 e5 2. Nf3 Nc6 3. Bc4 Nd4 4. Nxe5 Qg5 5. Nxf7 Qxg2 6. Nxh8 Qxh1+ 7. Bf1 Qxe4+ 8. Be2 Bc5 9. Nf7 Nf3+ 10. Kf1 Qh4 11. Nd6+ cxd6 12. Kg2 Qxf2+ 13. Kh3 Qxh2+ 14. Kg4 Nf6+ 15. Kxf3 Qf2#',
+
+  traxler_taking_the_bishop:
+    '1. e4 e5 2. Nf3 Nc6 3. Bc4 Nf6 4. Ng5 Bc5 5. Nxf7 Bxf2+ 6. Kxf2 Nxe4+ 7. Kg1 Qh4 8. Qe2 Nd4 9. Qe3 Nxc2 10. Qe2 Rf8 11. g3 Qe7 12. Qxe4 Nxa1 13. Kg2 d5 14. Qxd5 Rxf7 15. Qxf7+ Qxf7 16. Bxf7+ Kxf7 17. Rf1+ Kg8 18. Na3 b5 19. b3 b4 20. Nc4 Nc2 21. Bb2 Ba6 22. Rc1 Bxc4 23. Rxc2 Bd5+ 24. Kf2 c6 25. Bxe5 Rf8+ 26. Ke3 Re8 27. Kd4 Rd8 28. Rb2 Bf7+ 29. Ke3 Bg6 30. d4',
+
+  traxler_not_taking_the_bishop:
+    '1. e4 e5 2. Nf3 Nc6 3. Bc4 Nf6 4. Ng5 Bc5 5. Nxf7 Bxf2+ 6. Kf1 Qe7 7. Nxh8 d5 8. exd5 Nd4 9. d6 Qxd6 10. c3 Bg4 11. Qa4+ b5 12. Bxb5+ Nxb5 13. Kxf2 Bd7 14. Qd1 Qb6+ 15. Ke1 Nd6 16. Qf3 e4 17. Qe3 Qa6 18. b3 O-O-O 19. Rf1 Rxh8 20. c4',
+
+    // RCA https://youtu.be/Y6-RXOh50_w
+    rousseau_gambit: "1. e4 e5 2. Nf3 Nc6 3. Bc4 f5 4. Nc3 (4. exf5 e4 5. Nd4 (5. Qe2 Qe7 6. Ng1 Nf6 7. d3 Nd4 8. Qd1 Nxc2+ 9. Qxc2 exd3+) (5. Ng1 Nf6 6. d3 d5 7. Bb5 Bxf5 8. dxe4 Nxe4 9. Nf3 Bc5 10. O-O O-O 11. Nbd2 Nxf2 12. Rxf2 Bxf2+ 13. Kxf2 Nd4 14. Nxd4 Bxc2+) 5... Nf6 (5... Nxd4) 6. Nxc6 bxc6) (4. d3 Bc5 5. Ng5 f4 6. Nf7 Qh4 7. O-O Nf6 8. Nxh8 Ng4 9. h3 Nxf2 10. Rxf2 Qxf2+ 11. Kh2 (11. Kh1 f3 12. gxf3 d5 13. Bxd5 Bxh3) 11... f3) (4. d4 exd4 5. Nxd4 Nf6 6. Nxf5 d5 7. exd5 Bxf5 8. dxc6 Qxd1+ 9. Kxd1 O-O-O+) 4... fxe4 5. Nxe4 d5 6. Nxe5 Nxe5 7. Qh5+ Ng6 8. Bxd5 Nf6 9. Nxf6+ Qxf6 10. O-O c6 11. Bb3 Qf5 12. Re1+ Be7 13. Qxf5 Bxf5"
+};
+
+// https://www.youtube.com/watch?v=zoYH836DeyQ
+export var russian_game = {
+  stafford_gambit_rca_variation:
+    " 1. e4 e5 2. Nf3 Bc5 3. Nxe5 Nc6 { C40 King's Pawn Game: Busch-Gass Gambit, Chiodini Gambit } 4. Nxc6 dxc6 5. Bc4 Nf6 6. d3 Ng4 7. O-O Qh4 8. h3 Nxf2 9. Rxf2 Qxf2+ 10. Kh1 Bg4 11. Qxg4 Qg1# { Black wins by checkmate. } 0-1",
+};
